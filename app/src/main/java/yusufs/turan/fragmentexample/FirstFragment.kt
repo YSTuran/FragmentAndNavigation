6ffa86b39bd55ec9619ab71bfe830f6a618bd7d6
Yusuf S. Turan <siryusufsus@gmail.com>
2025-04-03 20:19:15 +0300

    view binding added

--- app/src/main/java/yusufs/turan/fragmentexample/FirstFragment.kt
+++ app/src/main/java/yusufs/turan/fragmentexample/FirstFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import yusufs.turan.fragmentexample.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
 
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,9 +20,23 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editText.setText("")
+        binding.button.setOnClickListener { nextFragment(it) }
+    }
+
+    fun nextFragment(view: View){
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
--- app/src/main/java/yusufs/turan/fragmentexample/SecondFragment.kt
+++ app/src/main/java/yusufs/turan/fragmentexample/SecondFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import yusufs.turan.fragmentexample.databinding.FragmentFirstBinding
+import yusufs.turan.fragmentexample.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +22,9 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
 
